39a27bbf3184f4e6332e453a1f8e57d3b782136a
Muhammad Agung Laksono <muh.agunglaksono@gmail.com>
2017-06-20 09:48:34 +0700

    upload pertama

---
+++ app/src/main/java/com/muhammadagung/pertemuan4_drawer/MainActivity.java
@@ -0,0 +1,131 @@
+package com.muhammadagung.pertemuan4_drawer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import static com.muhammadagung.pertemuan4_drawer.R.id.nav_Order;
+import static com.muhammadagung.pertemuan4_drawer.R.id.nav_beranda;
+import static com.muhammadagung.pertemuan4_drawer.R.id.nav_makanan;
+import static com.muhammadagung.pertemuan4_drawer.R.id.nav_minuman;
+import static com.muhammadagung.pertemuan4_drawer.R.id.nav_site;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id){
+            case  nav_beranda:
+                makanan makananFragment = new makanan();
+                android.support.v4.app.FragmentTransaction fragmentTransaction1 =
+                        getSupportFragmentManager().beginTransaction();
+                fragmentTransaction1.replace(R.id.FrameLayout, makananFragment);
+                fragmentTransaction1.commit();
+                getSupportActionBar().setTitle("Daftar Makanan");
+                break;
+
+            case nav_makanan:
+                minuman minumanFragment = new minuman();
+                android.support.v4.app.FragmentTransaction fragmentTransaction2 =
+                        getSupportFragmentManager().beginTransaction();
+                fragmentTransaction2.replace(R.id.FrameLayout, minumanFragment);
+                fragmentTransaction2.commit();
+                getSupportActionBar().setTitle("Daftar Minuman");
+                break;
+
+            case nav_minuman:
+                Toast.makeText(MainActivity.this, "Size and Price", Toast.LENGTH_SHORT).show();
+                break;
+
+            case nav_Order:
+                Toast.makeText(MainActivity.this, "Order", Toast.LENGTH_SHORT).show();
+                break;
+
+            case nav_site:
+                Intent intent = new Intent(MainActivity.this, ServerActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
